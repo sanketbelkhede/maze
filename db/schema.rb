984effd0bd9f7acfb331d51a9245d09fe689840e
ravi1728 <ravibhusari.99@gmail.com>
2023-07-22 10:47:18 +0000

    migrate RemoveDobFromArticles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_122507) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_22_104322) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "dob"
     t.integer "user_id", null: false
     t.boolean "privacy", default: false
     t.index ["user_id"], name: "index_articles_on_user_id"
